4b9b310fb87b29ee9479a6a97bf7e99f2dd69b45
thedevicelogon <thedevicelogon@gmail.com>
2026-03-15 01:16:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const http = require("http");
+
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+  res.end("Hello Node!\n");
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
